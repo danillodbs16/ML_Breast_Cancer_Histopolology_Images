47628acfd3846c76b24ea5baf899357828b21c07
danillodbs16 <danillo.dbs16@gmail.com>
2026-05-13 00:57:14 +0200

    changing requirements

--- BCML.py
+++ BCML.py
@@ -215,7 +215,7 @@ img {
 # =========================
 # LOAD MODEL
 # =========================
-model = load_model("./src/model/Refined_model_cw_v6.keras")
+model = load_model("/src/model/Refined_model_cw_v6.keras")
 
 
 # =========================
